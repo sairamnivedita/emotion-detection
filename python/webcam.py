ec584e108f333e0cfa1bc3ca3ba4fd742fbf7a54
nicolas.lupinski <nicolas.lupinski@gmail.com>
2011-03-24 15:28:06 +0000

    ajout webcam

---
+++ python/webcam.py
@@ -0,0 +1,46 @@
+import sys
+import cv
+ 
+# le main_loop cree une fenetre et y affiche l'image de la webcam, apres traitement (fonction passe en parametre)
+def main_loop(process = None):
+    print "Press ESC to exit ..."
+ 
+    # creation de la fenetre, redimensionnement automatique
+    cv.NamedWindow('Camera')
+ 
+    # webcam ou truc dans le genre
+    ###device = -1 # n'importe laquelle
+    capture = cv.CreateCameraCapture(0)
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 320)
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 240)    
+ 
+    # si pas de webcam...
+    if not capture:
+        print "Error opening capture device"
+        sys.exit(1)
+ 
+    while 1: 
+        # capture de l'image
+        frame = cv.QueryFrame(capture)
+        if frame is None:
+            break
+ 
+        # simulation d'un mirroir
+        cv.Flip(frame, None, 1)
+        
+        # traitement d'image
+        if process :
+            frame = process(frame)
+ 
+        # display webcam image
+        cv.ShowImage('Camera', frame)
+ 
+        # handle events
+        k = cv.WaitKey(10)
+        if k == 0x1b: # ESC
+            print 'ESC pressed. Exiting ...'
+            cv.DestroyAllWindows()
+            break  
+
+if __name__ == "__main__": 
+   main_loop()
